40d646becda3adaf2a8ef0d524ddf4125e978363
ArifRexa <arif.reza3126@gmail.com>
2023-06-09 00:26:43 +0600

    some minor change which is done for checking result

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ def main():
     user1.transfer(user2, 300)
     user1.take_loan(bank)
 
-    print(user1.check_balance())  # Output: 1000
-    print(user2.check_balance())  # Output: 600
+    print(user1.check_balance())
+    print(user2.check_balance())  
 
-    print(bank.get_total_balance())  # Output: 1700
-    print(bank.get_total_loan_amount())  # Output: 1000
+    print(bank.get_total_balance())  
+    print(bank.get_total_loan_amount())  
 
-    user1.view_transaction_history()  # Output: Deposited 500, Transferred 300 to Alice, Loan taken: 2000
-    user2.view_transaction_history()  # Output: Withdrew 200, Received 300 from John
+    user1.view_transaction_history()  
+    user2.view_transaction_history()  
 
 
 if __name__ == "__main__":
